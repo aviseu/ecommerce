2b23eba07ef2a3c6c786b5b4f2dbc0c9af04e046
alexv <alexandre@viseu.me>
2019-02-06 09:45:32 +0000

    Allow login with either email or username.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class LoginController extends Controller
 {
@@ -35,5 +38,27 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        $emailCredentials = $this->credentials($request);
+        $result = $this->guard()->attempt($emailCredentials, $request->filled('remember'));
+        if ($result) {
+            return true;
+        }
+
+        $usernameCredentials = [
+            'username' => $request->get('email'),
+            'password' => $request->get('password'),
+        ];
+
+        return $this->guard()->attempt($usernameCredentials, $request->filled('remember'));
     }
 }
